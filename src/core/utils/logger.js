b4d783fdbfb4c17a9b3dde150eda502d0586ab84
Nicolas Durand <nicolas.durand@sotiaf.fr>
2023-02-07 15:38:08 +0100

    :bug: fix logdna + env file

--- src/core/utils/logger.js
+++ src/core/utils/logger.js
@@ -1,36 +1,23 @@
 const fs = require("fs");
-const logdna = require("logdna");
 
 process.env.TZ = "Europe/Amsterdam";
 
 let infoStream = null;
 let log = null;
 
-if (process.env.NODE_ENV == "production") {
-	log = logdna.setupDefaultLogger(process.env.LOGDNA_KEY, {});
-} else {
-	infoStream = fs.createWriteStream("logs/app.log", { flags: "a+" });
-}
+infoStream = fs.createWriteStream("logs/app.log", { flags: "a+" });
 
 let logger = {
 	info: (msg) => {
 		if (msg instanceof Object) msg = JSON.stringify(msg);
 		let message = "INFO || " + new Date().toString() + " : " + msg + "\n";
-		if (process.env.NODE_ENV == "production") {
-			log.info(message);
-		} else {
-			infoStream.write(message);
-		}
+		infoStream.write(message);
 	},
 	exception: (msg) => {
 		console.log(msg);
 		if (msg instanceof Object) msg = JSON.stringify(msg);
 		let message = "EXCEPTION || " + new Date().toString() + " : " + msg + "\n";
-		if (process.env.NODE_ENV == "production") {
-			log.error(message);
-		} else {
-			infoStream.write(message);
-		}
+		infoStream.write(message);
 	},
 };
 
