7e0d5237e0226db2e3f936502c47e42d86f338a1
Nicolas Durand <nicolas.durand@sotiaf.fr>
2023-02-07 15:47:02 +0100

    :bug: remove log to fix error

--- src/core/utils/logger.js
+++ src/core/utils/logger.js
@@ -3,21 +3,20 @@ const fs = require("fs");
 process.env.TZ = "Europe/Amsterdam";
 
 let infoStream = null;
-let log = null;
 
-infoStream = fs.createWriteStream("logs/app.log", { flags: "a+" });
+//infoStream = fs.createWriteStream("logs/app.log", { flags: "a+" });
 
 let logger = {
 	info: (msg) => {
 		if (msg instanceof Object) msg = JSON.stringify(msg);
 		let message = "INFO || " + new Date().toString() + " : " + msg + "\n";
-		infoStream.write(message);
+		//infoStream.write(message);
 	},
 	exception: (msg) => {
 		console.log(msg);
 		if (msg instanceof Object) msg = JSON.stringify(msg);
 		let message = "EXCEPTION || " + new Date().toString() + " : " + msg + "\n";
-		infoStream.write(message);
+		//infoStream.write(message);
 	},
 };
 
